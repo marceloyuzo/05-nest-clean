ea50b82529c7672025ad4be5e08e683e791d21ad
marceloyuzo <marceloyuzo0@hotmail.com>
2025-02-03 12:28:03 -0300

    add tests e2e for create account, authenticate, create question and fetch recent questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
@@ -7,6 +6,7 @@ import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionsController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [
---
+++ src/controllers/create-question.e2e-spec.ts
@@ -0,0 +1,58 @@
+import { INestApplication } from '@nestjs/common'
+import { AppModule } from '@/app.module'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { PrismaService } from '@/prisma/prisma.service'
+import { JwtService } from '@nestjs/jwt'
+
+describe('Create question (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(AppModule)
+      .useValue(AppModule)
+      .compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '12345678',
+      },
+    })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    const response = await request(app.getHttpServer())
+      .post('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'New question',
+        content: 'Question content',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const questionOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: 'New question',
+        content: 'Question content',
+      },
+    })
+
+    expect(questionOnDatabase).toBeTruthy()
+  })
+})
