bdfee11830ecf3949ac2eac8e5eb54b202c4a890
marceloyuzo <marceloyuzo0@hotmail.com>
2025-02-05 15:22:43 -0300

    add new migrations prisma, implements methods prisma questions repository

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -8,7 +8,7 @@ import { QuestionBestAnswerChosenEvent } from '@/domain/forum/enterprise/events/
 
 export interface QuestionProps {
   authorId: UniqueEntityID
-  bestAnswerId?: UniqueEntityID
+  bestAnswerId?: UniqueEntityID | null
   title: string
   content: string
   slug: Slug
@@ -79,8 +79,16 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch()
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
     if (bestAnswerId === undefined) {
+      return
+    }
+
+    if (bestAnswerId === null) {
+      return
+    }
+
+    if (this.props.bestAnswerId === null) {
       return
     }
 
